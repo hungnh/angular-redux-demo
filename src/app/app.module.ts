aaad63d1f5a7498f8d3cce6760e2c987c5943104
Hung Nguyen <hungnh.uet@gmail.com>
2018-07-30 16:00:29 +0700

    adding Redux lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NgRedux, NgReduxModule} from '@angular-redux/store';
+import {IAppState, rootReducer} from './store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(rootReducer, {});
+  }
+}

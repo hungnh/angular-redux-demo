c4d9715bb87021aeb4fa3ff7e449405c9e467649
Hung Nguyen <hungnh.uet@gmail.com>
2018-07-31 17:03:05 +0700

    update recent todos

--- src/app/model/Todo.ts
+++ src/app/model/Todo.ts
@@ -1,9 +1,11 @@
 export class Todo {
   id: number;
   title: string;
+  isCompleted: boolean;
 
   constructor(id: number, title: string) {
     this.id = id;
     this.title = title;
+    this.isCompleted = false;
   }
 }
